0c3be5646fb5afe55b4d57c1253cf8a22817909f
Yaroslav Velianyk <yvelianyk@softjourn.com>
2017-05-11 17:12:15 +0300

    [SERVER] implement password checking in jwt auth strategy

--- handlers/jwtAuth.ts
+++ handlers/jwtAuth.ts
@@ -1,11 +1,19 @@
 import * as jwt from 'jwt-simple';
-import { config } from '../config';
-import { UserController } from '../controllers/user.controller';
+import {config} from '../config';
+import {UserController} from '../controllers/user.controller';
 
-const jwtAuth = function(req, res){
-    if (!(req.body.email && req.body.password)) {
+const jwtAuth = function (req, res): void {
+    let isPasswordInRequest = !!req.body.password;
+    let isEmailInRequest = !!req.body.email;
+    let user = UserController.findByEmail(req.body.email);
+    let isUserExists = !!user;
+    let isPasswordCorrect = user && user.password === req.body.password;
+
+    if (!isEmailInRequest || !isPasswordInRequest || !isUserExists || !isPasswordCorrect) {
         res.sendStatus(401);
+        return;
     }
+
     let token: string = jwt.encode(UserController.findByEmail(req.body.email), config.jwt.secret);
     UserController.updateUserToken({
         email: req.body.email,
@@ -14,4 +22,4 @@ const jwtAuth = function(req, res){
     res.json({token: token});
 };
 
-export { jwtAuth }
+export {jwtAuth}
